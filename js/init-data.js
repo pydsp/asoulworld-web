bf6a04eb44777dd0b9bae3a5a6b52d8cf48f743a
asoulworld <16623919+pydsp@users.noreply.github.com>
2021-10-14 11:15:11 +0800

    修复新版本中的部分问题

--- js/init-data.js
+++ js/init-data.js
@@ -123,10 +123,11 @@ $(window).on('load', function () {
             $("div#nav3").append(a)
         })
     })
-    requestLiveStatus()
     setInterval("requestLiveStatus()",GET_LIVING_STATUS_CYCLE*60000)
 })
-
+$(document).ready(function(){
+    requestLiveStatus()
+});
 Date.prototype.format = function (fmt) {
     let o = {
         "M+": this.getMonth() + 1, //月份
